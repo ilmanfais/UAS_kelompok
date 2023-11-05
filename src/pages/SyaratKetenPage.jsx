7603f59f05f57bda93f707380b3eeece733e33e3
M. FASRUL FAIS ILMAN <ilmanfaisocu26@gmail.com>
2023-11-05 13:29:36 +0700

    UAS

---
+++ src/pages/SyaratKetenPage.jsx
@@ -0,0 +1,112 @@
+import { Container, Row, Col  } from "react-bootstrap";
+import FaqComponent from "../components/FaqComponent";
+
+const SyaratKetenPage = () => {
+  return (
+    <div className="syarat-ketentuan-page">
+        <div className="syarat-ketentuan min-vh-100">
+            <Container>
+                <Row>
+                    <Col>
+                        <h1 className="fw-bold text-center mb-2 animate__animated animate__fadeInUp animate__delay-1s">Syarat & Ketentuan</h1>
+                    </Col>
+                </Row>
+                <Row className="pt-5">
+                    <Col>
+                        <p>
+                        Syarat dan Ketentuan ini merupakan perjanjian antara pengguna dan Maleo Grafis (“Kami”). Syarat dan Ketentuan ini mengatur Anda saat mengakses dan menggunakan seluruh layanan, fitur dan produk yang kami sediakan (untuk selanjutnya secara bersama-sama akan disebut sebagai “Platform”).
+                        </p>
+                        <p>
+                        Harap membaca Syarat dan Ketentuan ini secara seksama sebelum Anda mulai menggunakan Platform Kami, karena peraturan ini berlaku pada penggunaan Anda terhadap Platform Kami.
+                        </p>
+                    </Col>
+                </Row>
+                <Row className="py-3">
+                    <Col>
+                        <h4 className="fw-bold">1. Akun</h4>
+                        <p>
+                        Harap membaca Syarat dan Ketentuan ini secara seksama sebelum Anda mulai menggunakan Platform Kami, karena peraturan ini berlaku pada penggunaan Anda terhadap Platform Kami.
+                        </p>
+                        <p>
+                        Anda harus berusia minimal 18 (delapan belas) tahun atau sudah menikah dan tidak berada di bawah perwalian atau pengampuan agar Anda secara hukum memiliki kapasitas dan berhak untuk mengikatkan diri pada Syarat dan Ketentuan ini. Jika Anda tidak memenuhi ketentuan tersebut, maka tindakan Anda mendaftar, mengakses, menggunakan atau melakukan aktivitas lain dalam Platform Kami harus dalam sepengetahuan, pengawasan dan persetujuan yang sah dari orang tua atau wali atau pengampu Anda. Orang tua, wali atau pengampu yang memberikan persetujuan bagi Anda yang berusia di bawah 18 (delapan belas) tahun bertanggung jawab secara penuh atas seluruh tindakan Anda dalam penggunaan dan akses terhadap Platform.
+                        </p>
+                        <p>
+                        Sebelum menggunakan Platform, kami meminta Anda untuk menyetujui Syarat dan Ketentuan beserta Kebijakan Privasi untuk Anda dapat mendaftarkan diri Anda dengan memberikan informasi yang Kami butuhkan. Saat melakukan pendaftaran, Kami akan meminta Anda untuk memberikan akses akun google, nama lengkap, gambar profil, jenis kelamin dan alamat surat elektronik Anda. Kami juga dapat menghentikan penggunaan Platform jika dikemudian hari data-data yang Anda berikan kepada Kami terbukti tidak benar.
+                        </p>
+                        <p>
+                        Keamanan dan kerahasiaan akun Anda, termasuk namun tidak terbatas pada seluruh data pribadi yang Anda berikan kepada kami melalui formulir pendaftaran pada Platform kami, sepenuhnya merupakan tanggung jawab pribadi Anda. Segala kerugian dan risiko yang timbul akibat atau sehubungan dengan kelalaian Anda dalam menjaga keamanan dan kerahasiaan akun Anda ditanggung oleh Anda sendiri dan/atau orang tua, wali atau pengampu Anda (bagi Pengguna yang berada di bawah Usia Dewasa). Dengan demikian, kami akan menganggap setiap penggunaan atau pesanan yang dilakukan melalui akun Anda sebagai permintaan yang sah dari Anda.
+                        </p>
+                    </Col>
+                </Row>
+                <Row className="py-3">
+                    <Col>
+                        <h4 className="fw-bold">2. Akses terhadap Layanan Kami</h4>
+                        <p>
+                        Kami tidak menjamin bahwa Platform Kami, maupun konten di dalamnya, akan selalu tersedia atau tanpa gangguan. Kami memberikan layanan akses materi secara lifetime dengan syarat, selama Maleo Grafis masih beroperasi. Kami akan menghentikan layanan akses materi jika terjadi Force Majure, seperti Peperangan, Huru Hara, Kebakaran, Bencana Alam dan Pailit. Kami dapat menangguhkan, menarik, memberhentikan, maupun mengganti bagian mana pun dari Platform Kami tanpa pemberitahuan sebelumnya. Kami tidak bertanggung jawab atas alasan apa pun yang membuat Platform Kami tidak tersedia pada waktu atau periode tertentu. Dan Mohon diketahui, bahwa kelas yang telah Anda beli tidak dapat dibatalkan atau diuangkan kembali (refund).
+                        </p>
+                    </Col>
+                </Row>
+                <Row className="py-3">
+                    <Col>
+                        <h4 className="fw-bold">3. Akses terhadap Konsultasi Mentor</h4>
+                        <p>
+                        Ketika Mentor mengundurkan diri, kami masih memberikan waktu selama 3 bulan untuk para member dapat berkonsultasi dengan mentor, baik di dalam grup maupun melalui Direct Message Telegram. Setelah 3 bulan berlalu, maka layanan konsultasi bersama mentor akan kami hapuskan bersamaan dengan Update Materi runtin di dalam kelas tersebut. Grup telegram tidak akan kami hapus agar para member masih dapat berdiskusi dan saling bertukar informasi secara pribadi. Apakah kami memberikan mentor pengganti? jawabannya adalah Tidak. 
+                        </p>
+                    </Col>
+                </Row>
+                <Row className="py-3">
+                    <Col>
+                        <h4 className="fw-bold">4. Penggunaan yang Dilarang</h4>
+                        <p>
+                        Anda hanya diperbolehkan menggunakan Platform kami untuk tujuan-tujuan yang sah menurut hukum. Anda dilarang keras untuk menggunakan Platform Kami:
+                        </p>
+                        <ul>
+                            <li>Dengan cara-cara yang melanggar hukum dan peraturan lokal, nasional, maupun internasional yang berlaku.</li>
+                            <li> Dengan cara-cara yang melanggar hukum atau menipu, atau memiliki tujuan atau dampak yang melanggar hukum atau menipu.</li>
+                            <li>Untuk tujuan membahayakan atau mencoba mencelakakan anak di bawah umur dengan cara apa pun.</li>
+                            <li>Menyebarkan atau mengirimkan materi iklan atau promosi yang tidak diinginkan atau tidak sah, serta bentuk permintaan serupa lainnya (seperti spam).</li>
+                            <li>Dengan sengaja meneruskan data, mengirim atau mengunggah materi yang mengandung virus, trojan, worm, logic bomb, keystroke loggers, spyware, adware, maupun program berbahaya lainnya atau kode komputer sejenis yang dirancang untuk memberikan efek merugikan terhadap pengoperasian perangkat lunak atau perangkat keras apa pun.</li>
+                        </ul>
+                        <p>
+                        Anda juga setuju :
+                        </p>
+                        <ul>
+                            <li>Untuk tidak mengirim, menerima, mengunggah, mengunduh, mereproduksi, menggandakan, menyalin, menggunakan atau memperjualbelikan materi, konten atau bagian mana pun dari Platform Kami yang bertentangan dengan ketentuan dalam Syarat dan Ketentuan Platform Kami.</li>
+                            <li>Untuk tidak mengakses tanpa izin, mengganggu, merusak, atau mengacak-acak:</li>
+                            <li>bagian mana pun dari Platform Kami;</li>
+                            <li>peralatan atau jaringan di mana Platform Kami tersimpan;</li>
+                            <li>perangkat lunak apa pun yang digunakan dalam penyediaan Platform Kami; atau</li>
+                            <li>peralatan atau jaringan atau perangkat lunak yang dimiliki oleh pihak ketiga mana pun.</li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row className="py-3">
+                    <Col>
+                        <h4 className="fw-bold">5. Tanggung Jawab Anda</h4>
+                        <p>
+                        Anda bertanggung jawab secara penuh atas setiap kerugian dan/atau klaim yang timbul dari penggunaan Platform melalui akun Anda, baik oleh Anda atau pihak lain yang menggunakan akun Anda, dengan cara yang bertentangan dengan Ketentuan Penggunaan ini, peraturan perundang-undangan yang berlaku, pelanggaran hak kekayaan intelektual dan/atau aktivitas lain yang merugikan publik dan/atau pihak lain manapun atau yang dapat atau dianggap dapat merusak reputasi kami.
+                        </p>
+                        <p>
+                        Kami tidak menjamin bahwa Platform akan aman atau terbebas dari bug atau virus. Anda bertanggung jawab untuk mengatur teknologi informasi, program komputer, serta platform yang Anda gunakan untuk mengakses Platform kami. Anda harus menggunakan perangkat lunak anti-virus Anda sendiri.
+                        </p>
+                    </Col>
+                </Row>
+                <Row className="py-3">
+                    <Col>
+                        <h4 className="fw-bold">6. Hukum yang Berlaku</h4>
+                        <p>
+                        Syarat dan Ketentuan ini diatur berdasarkan hukum Republik Indonesia. Setiap dan seluruh perselisihan yang timbul dari penggunaan Platform tunduk pada yurisdiksi eksklusif Pengadilan Negeri Kota Jombang.
+                        </p>
+                        <p>
+                        Maleo Grafis akan melaporkan setiap pelanggaran plagiasi konten dan memproses secara hukum.
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+        <FaqComponent />
+    </div>
+  )
+}
+
+export default SyaratKetenPage
